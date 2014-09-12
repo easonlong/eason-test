9ff467b0753d0b99901f0edd8446e07e43ccee54
eason <yaokunlong@gmail.com>
2014-09-12 14:02:08 +0800

    push

--- src/main/java/com/eason/coding/life/string/TestStringUtils.java
+++ src/main/java/com/eason/coding/life/string/TestStringUtils.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class TestStringUtils {
     @Test
     public void testSubstringBefore(){
-        String before="CMBX-123456-123";
+        String before="CMBX-123456-123456";
         String after=StringUtils.substringBefore(before, "-");
         System.out.println(after);
     }
