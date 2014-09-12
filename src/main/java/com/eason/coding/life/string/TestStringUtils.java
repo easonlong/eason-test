1c629ca7b4b99f9ffac9df83f414318f4a5420d7
yl47784 <yl47784@GCOTDVMMW741199.nam.nsroot.net>
2014-09-12 11:54:19 +0800

    test StringUtils

---
+++ src/main/java/com/eason/coding/life/string/TestStringUtils.java
@@ -0,0 +1,13 @@
+package com.eason.coding.life.string;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+
+public class TestStringUtils {
+    @Test
+    public void testSubstringBefore(){
+        String before="CMBX-123456-123";
+        String after=StringUtils.substringBefore(before, "-");
+        System.out.println(after);
+    }
+}
