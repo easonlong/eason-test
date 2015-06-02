9c46d00ca5f163d25c5d5b038390576429fd441f
longyaokun <longyaokun@qq.com>
2015-06-02 18:01:19 +0800

    json test

---
+++ src/main/java/com/eason/coding/life/json/JsonTest.java
@@ -0,0 +1,16 @@
+package com.eason.coding.life.json;
+
+import net.sf.json.JSONObject;
+
+public class JsonTest {
+	public static void main(String[] args) {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("name", "Eason");
+		jsonObject.put("age", 12);
+		System.out.println(jsonObject.toString());
+		
+		String jsonString="{\"name\":\"Eason\",\"age\":12}";
+		JSONObject jsonObject2=JSONObject.fromObject(jsonString);
+		System.out.println(jsonObject2.toString());
+	}
+}
