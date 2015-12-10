d7d890e817a9c4fcac8d498373356a94f324fef0
longyaokun <longyaokun@didichuxing.com>
2015-12-10 19:24:38 +0800

    test

--- src/main/java/com/eason/coding/life/aop/SimpleService.java
+++ src/main/java/com/eason/coding/life/aop/SimpleService.java
@@ -1,13 +1,25 @@
 package com.eason.coding.life.aop;
 
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
 
+@Component("service")
 public class SimpleService implements Service {
 	private final Logger LOGGER = Logger.getLogger(SimpleService.class);
 
+	private String name;
+	
+	public SimpleService(){
+		LOGGER.info("default constructor");
+	}
+	
+	public SimpleService(String name){
+		LOGGER.info("constructor with arg");
+		this.name = name;
+	}
 	@Override
 	public void doService(String message) {
-		LOGGER.info("Processing the message:" + message);
+		LOGGER.info(this.name + " is processing the message:" + message);
 	}
 
 }
