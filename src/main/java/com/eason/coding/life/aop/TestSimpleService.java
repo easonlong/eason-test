245154efc3ac6f7f9402f74c454551f4094a9b45
longyaokun <longyaokun@didichuxing.com>
2015-12-14 11:28:07 +0800

    add shutdown hook

--- src/main/java/com/eason/coding/life/aop/TestSimpleService.java
+++ src/main/java/com/eason/coding/life/aop/TestSimpleService.java
@@ -1,15 +1,49 @@
 package com.eason.coding.life.aop;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 public class TestSimpleService {
 	private static final String[] APPLICATION_CONTEXT_PATH = { "spring/spring-aop-aspectj.xml" };
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext ac = new ClassPathXmlApplicationContext(
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(
 				APPLICATION_CONTEXT_PATH);
+		Runtime.getRuntime().addShutdownHook(new Shutdown(ac));
 		Service service = (Service) ac.getBean("service");
 		service.doService("Hello, World!");
+	}
+	private static void wait(ClassPathXmlApplicationContext context) throws InterruptedException {
+		synchronized (context) {
+			while (context.isActive()) {
+				System.out.println("wait");
+				context.wait();
+			}
+		}
+	}
+	private static class Shutdown extends Thread {
+
+		final ClassPathXmlApplicationContext context;
+
+		public Shutdown(ClassPathXmlApplicationContext context) {
+			super();
+			this.context = context;
+		}
+
+		@Override
+		public void run() {
+			synchronized (this.context) {
+				try {
+	                Thread.sleep(1000);
+                } catch (InterruptedException e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+                }
+				System.out.println("shutdown");
+				// 先关闭后唤醒
+				this.context.close();
+				this.context.notifyAll();
+			}
+		}
 	}
 }
